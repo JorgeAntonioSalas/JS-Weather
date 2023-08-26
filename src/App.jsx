001c14900bb425e4f460154f3a32bfa738cb611e
bri <jorge.salas.sheron@gmail.com>
2023-08-25 20:52:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Background from './Components/Background'
+import Weather from './Components/Weather'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Background/>
+      <Weather/>
+    </div>
+    
+  )
+}
+
+export default App
